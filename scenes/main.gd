7caa224c13df689d84505758f5a089654fcc1ab9
Tay <videogamemarth@gmail.com>
2024-10-01 23:17:38 -0400

    Added HUD

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,9 +4,11 @@ const KNIGHT_START_POS := Vector2i(176, 373) #176, 376
 const CAM_START_POS := Vector2i(576,324)
 
 var speed : float
-const START_SPEED : float = 10.0
+var score : int
+const START_SPEED : float = 8.0
 const MAX_SPEED : int = 20
 var screen_size : Vector2i
+var game_running : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,15 +16,35 @@ func _ready() -> void:
 	new_game()
 
 func new_game():
+	#reset var
+	game_running = false
+	score = 0
+	show_score()
+	
+	#reset nodes
 	$Knight.position = KNIGHT_START_POS
 	$Knight.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0,376)
+	
+	#reset HUD
+	$HUD.get_node("StartLabel").show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#moves knight, camera, and ground
-	$Knight.position.x += START_SPEED
-	$Camera2D.position.x += START_SPEED
-	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+	if game_running:
+		#moves knight, camera, and ground
+		$Knight.position.x += START_SPEED
+		$Camera2D.position.x += START_SPEED
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+			
+		score += START_SPEED
+		#print(score/40)
+		show_score()
+	else:
+		if Input.is_action_just_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
+func show_score():
+	$HUD.get_node("ScoreLabel").text = "Score: " + str(score/40)
--- scripts/knight.gd
+++ scripts/knight.gd
@@ -1,17 +1,20 @@
 extends CharacterBody2D
 
-const GRAVITY = 5000.0
-const JUMP_SPEED = -1800.0
+const GRAVITY = 4000.0
+const JUMP_SPEED = -1400.0
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y = JUMP_SPEED
-		elif Input.is_action_pressed("ui_down"):
-			$AnimatedSprite2D.play("duck")
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("idle")
 		else:
-			$AnimatedSprite2D.play("run")
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = JUMP_SPEED
+			elif Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("duck")
+			else:
+				$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("jump")
 
